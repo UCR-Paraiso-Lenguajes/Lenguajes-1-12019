5636981e4a7001ce930c487970e9315a55971fae
Samuel <samsserranoguerra@gmail.com>
2019-03-19 19:26:35 -0600

    Laboratorio1
    Spring.
    Insertar y Mostrar Empleados.

---
+++ B56860/Labs/lab1/EmpleadosController.java
@@ -0,0 +1,22 @@
+package cr.ac.ucr.ie.app.controllers.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import cr.ac.ucr.ie.app.domain.Empleado;
+import cr.ac.ucr.ie.app.domain.Empleados;
+
+
+@RestController
+public class EmpleadosController 
+{
+	private Empleados empleados = new Empleados();
+
+	
+	@RequestMapping("/empleados")
+	public @ResponseBody Iterable<Empleado> listar() 
+	{
+		return empleados.listar();
+	}
+}
