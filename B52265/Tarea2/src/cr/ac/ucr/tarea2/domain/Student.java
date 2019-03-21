ea9df6b25942761c0e3e906f6092bee666d8238b
deltadragon <andres.coto3539@gmail.com>
2019-03-21 16:00:06 -0600

    una detalles agregados a la Tarea 2

--- B52265/Tarea2/src/cr/ac/ucr/tarea2/domain/Student.java
+++ B52265/Tarea2/src/cr/ac/ucr/tarea2/domain/Student.java
@@ -10,8 +10,12 @@ public class Student {
      */
     private int id;
     private String name;
+    /*Tambien usando el tip Use pronunceable Names, las variables,
+    son palabras conocidas y faciles de pronunciar*/
     private String apellido;
-    private ArrayList<Curso> cursos = new ArrayList<>();
+    /*Aca evitamos la desinformacion, ya que efectivamente listaCursos es una lista, y tambien se coloco
+    listas para que como dije se sepan que es una lista no solo un grupo de cursos*/
+    private ArrayList<Curso> listaCursos = new ArrayList<>();
 
     public Student() {
     }
@@ -46,4 +50,26 @@ public class Student {
         this.apellido = lastName;
     }
 
+    /*Aca hacemos una distincion en los metodos de matricula con sentido donde sabemos perfectamente
+    cual metodo debemos invocar para cual situacion sin causar confuciones*/
+    public void matriculaOrdinaria(Curso curso) throws Exception {
+        /*tip small es un metodo sencillo y rapido sin mucha cosa, evitando muchos niveles de validaciones
+         */
+
+        /*siguiento el tip block and identing la validacion if
+            contiene un metodo en su validacion se sabe que hace el metodo por el nombre y que se esta
+            evaluando ademas de su correcta identacion*/
+        if (this.listaCursos.contains(curso)) {
+            throw new Exception("El curso ya existe");
+        } else {
+            this.listaCursos.add(curso);
+        }
+        /*manteniendo el tip do one thing, el metodo solo hace una cosa que es matricular, no realiza
+        mas acciones o validaciones*/
+        /*Tambien mantenemos un solo nivel de abstracion por funcion*/
+    }
+
+    /*Este asi como el metodo anterior se manejan nombres descriptivos sabemos exactamente que hace*/
+    public void matriculaInclusion() {
+    }
 }
