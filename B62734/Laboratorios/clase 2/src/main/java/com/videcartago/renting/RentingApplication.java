fc830d0fa6fcb4018a520133d91824a790ababd2
Daniel fonseca Solano <31803434+dfs11@users.noreply.github.com>
2019-03-21 20:28:01 -0600

    clase2

---
+++ B62734/Laboratorios/clase 2/src/main/java/com/videcartago/renting/RentingApplication.java
@@ -0,0 +1,13 @@
+package com.videcartago.renting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentingApplication.class, args);
+	}
+}
+
