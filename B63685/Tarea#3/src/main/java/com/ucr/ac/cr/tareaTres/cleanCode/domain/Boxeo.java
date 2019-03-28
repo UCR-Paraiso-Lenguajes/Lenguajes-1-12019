f6d41f0756590c36ee477466fc05ffac758ee846
Alfonsinius <43684079+Alfonsinius@users.noreply.github.com>
2019-03-28 04:09:49 -0600

    B63685-Tarea#3

---
+++ B63685/Tarea#3/src/main/java/com/ucr/ac/cr/tareaTres/cleanCode/domain/Boxeo.java
@@ -0,0 +1,112 @@
+package com.ucr.ac.cr.tareaTres.cleanCode.domain;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Boxeo implements Deporte{
+
+	private int idBoxeo;
+	private int cantidadJugadores;
+	private String nombreJugador;
+	private String apellidosJugador;
+	private int edadJugador;
+	private String reglasDelJuego;
+	private int tamañoRing;
+	
+
+	/*
+	Abstraction, capítulo 6, pagina 93
+	
+	En este caso se implemento deporte a boxeo. Una mala práctica hubiera sido no hacer una interface.
+	Entonces en el caso de querer crear otro deporte (ejemplo football) se necesitaría repetir los 
+	metodos, ya que todos comparten los mismos, por lo que lo correcto es implementar una interface.
+	 
+	 Nota: Esta clase sirve perfectamente para mostrar el ejemplo, sin embargo, se utlizará para
+	 ejemplos posteriores.
+	 */
+	
+	
+	public Boxeo(int idBoxeo, int cantidadJugadores, String nombreJugador, String apellidosJugador, int edadJugador,
+			String reglasDelJuego) {
+		
+		if(cantidadJugadores <= 0) throw new RuntimeException("La cantidad de jugadores debe ser mayor a 0.");
+		if(nombreJugador == null || nombreJugador.trim().equals("")) throw new RuntimeException("El nombre es requerido");
+		if(apellidosJugador == null || apellidosJugador.trim().equals("")) throw new RuntimeException("Los apellidos son requeridos");
+		
+		this.cantidadJugadores = cantidadJugadores;
+		this.nombreJugador = nombreJugador;
+		this.apellidosJugador = apellidosJugador;
+		this.edadJugador = edadJugador;
+		this.reglasDelJuego = reglasDelJuego;
+		this.idBoxeo = idBoxeo;
+		
+		
+	}
+	
+	public Boxeo() {
+		
+	}
+
+	public String getNombreJugador() {
+		
+		return nombreJugador;
+	}
+
+
+	public String apellidosJugador() {
+		
+		return apellidosJugador;
+	}
+
+
+	public int cantidadJugadores() {
+		
+		return cantidadJugadores;
+	}
+
+
+	public int edadJugador() {
+		
+		return edadJugador;
+	}
+	
+	public String reglasDelJuego() {
+		
+		return reglasDelJuego;
+	}
+	
+	
+	public int idDeporte() {
+		return idBoxeo;
+	}
+
+	public int getTamañoRing() {
+		return tamañoRing;
+	}
+
+	/*
+	 Data Transfer Objects
+	 Capítulo 6, page 100
+	 */
+	public void setTamañoRing(int tamañoRing) {
+		this.tamañoRing = tamañoRing;
+	}
+
+	public ArrayList<Integer> listaAsientosEspectadores(ArrayList<Integer> asientos) {
+		/*
+		 Don’t Return Null capítulo 7 página 110
+		 */
+		//En este caso se retornó un nulo al implementar el método, si no se corrige puede ser un problema después
+		if(asientos == null) {
+			return null;
+		}
+		else {
+			return asientos;
+		}
+	}
+
+	
+	
+	
+}
---
+++ B63685/Tarea#3/src/main/java/com/ucr/ac/cr/tareaTres/cleanCode/domain/BoxeoData.java
@@ -0,0 +1,154 @@
+package com.ucr.ac.cr.tareaTres.cleanCode.domain;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BoxeoData {
+
+	private ArrayList<Boxeo> boxeos;
+	private ArrayList<String> contrincantes;
+	private ArrayList<String> equipamientos;
+
+	public BoxeoData(ArrayList boxeos) {
+
+		if (boxeos == null)
+			throw new RuntimeException("No puede ser nulo, debe agregar un campeonato de boxeo");
+
+		if (contrincantes == null)
+			throw new RuntimeException("contrincantes no puede ser nulo");
+
+		this.boxeos = boxeos;
+	}
+	/*
+	 * 
+	 * Data/Object Anti-symmetric, Capítulo 6, Página 95
+	 */
+
+	// Orientado a objetos
+
+	public void agregarBoxeador(Boxeo boxeo) {
+
+		if (boxeo == null)
+			throw new RuntimeException("El nombre no debe ser nulo.");
+
+		else {
+			boxeos.add(boxeo);
+		}
+
+	}
+
+	/*
+	 * Data Structures En este caso se irían agregando uno por uno lo jugadores, lo
+	 * que expone los datos como se menciona en el libro. (capítulo 6, página 96,
+	 * parrafo 2)
+	 */
+	public String agregaBoxeador() {
+
+		int idBoxeo = 1;
+		int cantidadJugadores = 10;
+		String nombreJugador = "Alfonso";
+		String apellidosJugador = "Jiménez";
+		int edadJugador = 22;
+		String reglasDelJuego = "No se permiten golpes bajos.";
+
+		return " " + idBoxeo + " " + cantidadJugadores + " " + nombreJugador + apellidosJugador + " " + edadJugador
+				+ " " + reglasDelJuego;
+
+	}
+
+	/*
+	 * The law of demeter capítulo 6, página 97
+	 */
+	// Incorrecto
+	public boolean compruebaNombre(boolean validaNombre) {
+
+		Boxeo boxeo = new Boxeo();
+
+		validaNombre = boxeo.getNombreJugador().trim().equals("");
+		return validaNombre;
+
+	}
+
+	// Correcto
+	/*
+	 * Tomado de: Capítulo 6, Página 98
+	 */
+	public boolean validaNombre(boolean nombreValidado) {
+
+		Boxeo boxeo = new Boxeo();
+
+		String nombreBoxeador = boxeo.getNombreJugador();
+		String quitaEspacios = nombreBoxeador.trim();
+		nombreValidado = quitaEspacios.equals("");
+
+		return nombreValidado;
+	}
+
+	/*
+	 * Use Exceptions Rather Than Return Codes and Write Your Try-Catch-Finally
+	 * Statement First Capítulo 7, página 104-105 En este caso se puede arrojar una
+	 * exception si se pone null en alguno de los nombres
+	 */
+
+	public void agregarContrincantes(String contrincanteUno, String contrincanteDos) {
+
+		Logger logger = Logger.getLogger("MyLogger");
+		contrincanteUno = null;
+		boolean sonElMismo = false;
+		try {
+
+			if (contrincanteUno.equals(contrincanteDos)) {
+
+				logger.log(Level.INFO, "Los contrincantes tienen el mismo nombre");
+			}
+
+		} catch (Exception e) {
+			logger.log(Level.WARNING, "ERROR, se insertó un nulo");
+		} finally {
+			logger.log(Level.INFO, "Contrincante Uno" + contrincanteUno + "ContrincanteDos" + contrincanteDos);
+		}
+
+	}
+
+	// UseUncheckedExceptions capitulo 7, página 106
+	public void uncheckedExceptions() {
+
+		Logger logger = Logger.getLogger("MyLogger");
+		int x = 0;
+		int y = 10;
+		int z = y / x;
+		logger.log(Level.INFO, "" + z);
+	}
+
+	/*
+	 * Provide Context with Exceptions capítulo 7, página 107
+	 */
+	public void calculoEquipoDeBoxeo(int guantes, int cascos) {
+
+		Boxeo boxeo = new Boxeo();
+		Logger logger = Logger.getLogger("MyLogger");
+		int equipamiento = 0;
+		try {
+			equipamiento = (guantes + cascos) / boxeo.cantidadJugadores();
+		} catch (Exception e) {
+			logger.log(Level.WARNING, "Se ha realizado una división entre 0!!!");
+		} finally {
+			logger.log(Level.INFO, "" + equipamiento);
+		}
+
+	}
+	/*
+	 * Don’t Pass Null capítulo 7, página 111 En este caso si se le inserta un nulo
+	 * daría un NullPointerException
+	 */
+
+	public ArrayList<String> agregarEquipamiento(String equipamiento) {
+
+		equipamientos.add(equipamiento);
+
+		return equipamientos;
+
+	}
+
+}
