7d3eb839844718391c2b9d2fd97810fac7e6c029
Nicolefonseca98 <37601556+Nicolefonseca98@users.noreply.github.com>
2019-03-28 21:32:13 -0600

    Cambios lab, B62722

--- B62722/Laboratorios/LaboratorioLibreria/Orbis/src/main/java/com/orbis/b62722/domain/Libro.java
+++ B62722/Laboratorios/LaboratorioLibreria/Orbis/src/main/java/com/orbis/b62722/domain/Libro.java
@@ -1,6 +1,8 @@
 package com.orbis.b62722.domain;
 
 import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Libro {
 	
@@ -8,63 +10,66 @@ public class Libro {
 	private String titulo;
 	private int año;
 	private float precio;
-	private int id_editorial;
+	private Editorial editorial;
+	private List<Autor> autores;
 	
-	public Libro(int id_libro, String titulo, int año, int precio, int id_editorial) {
-		Calendar fechaActual = Calendar.getInstance();
-		
-		if(id_libro <= 0) throw new RuntimeException("El id debe ser mayor a 0.");
-		if(titulo == null || titulo.trim().equals("")) throw new RuntimeException("El título es requerido");
-		if(año >= fechaActual.YEAR) throw new RuntimeException("La fecha es incorrecta.");
-		if(precio <= 0) throw new RuntimeException("El precio debe ser mayor a 0.");
-		if(id_editorial <= 0) throw new RuntimeException("El id debe ser mayor a 0.");
-		
-		this.id_libro = id_libro;
-		this.titulo = titulo;
-		this.año = año;
-		this.precio = precio;
-		this.id_editorial = id_editorial;
+	public Libro() {
+		autores = new LinkedList<Autor>();
+		editorial = new Editorial();
 	}
-	
+
 	public int getId_libro() {
 		return id_libro;
 	}
-	
+
 	public void setId_libro(int id_libro) {
+		if(id_libro <= 0) throw new RuntimeException("El id debe ser mayor a 0.");
 		this.id_libro = id_libro;
 	}
-	
+
 	public String getTitulo() {
 		return titulo;
 	}
-	
+
 	public void setTitulo(String titulo) {
+		if(titulo == null || titulo.trim().equals("")) throw new RuntimeException("El título es requerido");
 		this.titulo = titulo;
 	}
-	
+
 	public int getAño() {
 		return año;
 	}
-	
+
 	public void setAño(int año) {
 		this.año = año;
 	}
-	
+
 	public float getPrecio() {
 		return precio;
 	}
-	
+
 	public void setPrecio(float precio) {
+		if(precio <= 0) throw new RuntimeException("El precio debe ser mayor a 0.");
 		this.precio = precio;
 	}
-	
-	public int getId_editorial() {
-		return id_editorial;
+
+	public Editorial getEditorial() {
+		return editorial;
+	}
+
+	public void setEditorial(Editorial editorial) {
+		this.editorial = editorial;
+	}
+
+	public List<Autor> getAutores() {
+		return autores;
+	}
+
+	public void setAutores(List<Autor> autores) {
+		this.autores = autores;
 	}
 	
-	public void setId_editorial(int id_editorial) {
-		this.id_editorial = id_editorial;
-	}
+	
 	
 	
 
