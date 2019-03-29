97d43777f9a0ddfb2e6c78941d0b8639216b7d32
Melissa <meli8538@gmail.com>
2019-03-28 21:02:22 -0600

    Laboratorio1: aplicación orbis

---
+++ B76223Laboratorio1/orbis/src/main/java/com/orbis/B76223/ventas/domain/Libro.java
@@ -0,0 +1,56 @@
+package com.orbis.B76223.ventas.domain;
+
+import java.util.List;
+
+public class Libro {
+
+	private int idLibro; 
+	private String titulo;
+	private int ano; 
+	private float precio; 
+	private Editorial editorial; 
+	private List<Autor> autores;
+	
+	
+	public int getIdLibro() {
+		return idLibro;
+	}
+	public void setIdLibro(int idLibro) {
+		this.idLibro = idLibro;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public Editorial getEditorial() {
+		return editorial;
+	}
+	public void setEditorial(Editorial editorial) {
+		this.editorial = editorial;
+	}
+	public List<Autor> getAutores() {
+		return autores;
+	}
+	public void setAutores(List<Autor> autores) {
+		this.autores = autores;
+	}
+	
+	
+	
+}
+
+
