191c591d96bdab08a0995a44e9c79a58366c80d1
JesusTorresRodriguez <felipe-torres23@hotmail.com>
2019-04-02 21:23:27 -0600

    Clases 2 B67156
    Clase 2
    Ejercicio renting de peliculas, nuevos requerimientos

---
+++ B67156/Clases/renting/src/main/java/com/videocartago/renting/business/PeliculaBusiness.java
@@ -0,0 +1,26 @@
+package com.videocartago.renting.business;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.videocartago.renting.data.PeliculaData;
+import com.videocartago.renting.domain.Pelicula;
+
+@Service
+public class PeliculaBusiness {
+	
+	@Autowired
+	private PeliculaData peliculaDao;
+	
+	public List<Pelicula> findAllMoviesByTitleAndGenre(String title, String genre) {
+		return peliculaDao.findMoviesByTitleAndGenre(title, genre);
+	}
+	
+	public Pelicula save(Pelicula pelicula) throws SQLException{
+		return peliculaDao.save(pelicula);
+	}
+
+}
