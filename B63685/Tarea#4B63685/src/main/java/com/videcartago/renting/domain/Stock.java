66263f982c9792437acdb7d4e91b8311b120da6e
Alfonsinius <43684079+Alfonsinius@users.noreply.github.com>
2019-04-14 11:19:49 -0600

    Tarea4B63685
    Tarea cuatro finalizada

---
+++ B63685/Tarea#4B63685/src/main/java/com/videcartago/renting/domain/Stock.java
@@ -0,0 +1,41 @@
+package com.videcartago.renting.domain;
+
+public class Stock extends Estado{
+
+	@Override
+	public void devolver(Pelicula pelicula) {
+		
+		pelicula.setEstado(this);
+	}
+
+	
+	@Override
+	public void rentar(Pelicula pelicula) {	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ B63685/Tarea#4B63685/src/main/java/com/videocartago/renting/factory/Clasificacion.java
@@ -0,0 +1,20 @@
+package com.videocartago.renting.factory;
+
+import com.videcartago.renting.domain.Pelicula;
+
+public abstract class Clasificacion{
+
+			
+	public abstract String tipoClasificacion();
+	
+	public abstract Pelicula getPelicula();
+		
+
+	
+	
+	
+	
+	
+	
+
+}
