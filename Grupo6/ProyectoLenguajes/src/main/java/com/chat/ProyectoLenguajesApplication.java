aca1bd220ac8a3949f7876f1d2533aa7f9342408
leoenterprise94 <42311754+leoenterprise94@users.noreply.github.com>
2019-05-08 00:32:42 -0600

    Commit Estructura Inicial
    Contiene interfaz login y properties de conexion

---
+++ Grupo6/ProyectoLenguajes/src/main/java/com/chat/ProyectoLenguajesApplication.java
@@ -0,0 +1,13 @@
+package com.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLenguajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLenguajesApplication.class, args);
+	}
+
+}
