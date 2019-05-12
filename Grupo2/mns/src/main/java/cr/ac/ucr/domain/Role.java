643e7a7e8685ffb00611f26a7bdaad18ee0c3061
danii230 <37677168+danii230@users.noreply.github.com>
2019-05-12 16:02:38 -0500

    domain
    domain

--- Grupo2/mns/src/main/java/cr/ac/ucr/domain/Role.java
+++ Grupo2/mns/src/main/java/cr/ac/ucr/domain/Role.java
@@ -2,4 +2,7 @@ package cr.ac.ucr.domain;
 
 public class Role {
 
+	private int idRole;
+	private String detalle;
+	
 }
--- Grupo2/mns/src/main/java/cr/ac/ucr/domain/User.java
+++ Grupo2/mns/src/main/java/cr/ac/ucr/domain/User.java
@@ -1,7 +1,16 @@
 package cr.ac.ucr.domain;
 
+import java.awt.Image;
+import java.util.ArrayList;
+
 public class User {
 
-	
+	private int idUser;
+	private String name;
+	private String hash;
+	private String email;
+	private Image avatar;
+	private ArrayList<Role> roleList;
+	private ArrayList<Room> listRoom;
 	
 }
