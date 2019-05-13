19357642df51f0a76a06699f88677136b26d423f
Lyka <gitsune4406@gmail.com>
2019-05-12 23:15:12 -0600

    Room y User
    Domain, Form y Data

---
+++ Grupo6/ProyectoLenguajes/src/main/java/com/chat/domain/User.java
@@ -0,0 +1,38 @@
+package com.chat.domain;
+
+public class User {
+
+	private int id;
+	private String password;
+
+	public User() {
+		super();
+	}
+
+	public User(int id, String password) {
+		super();
+		this.id = id;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
+	
+	
+}
