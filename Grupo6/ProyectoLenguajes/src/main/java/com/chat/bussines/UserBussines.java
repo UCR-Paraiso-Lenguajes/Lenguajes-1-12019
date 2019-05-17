9546e4001017ab7be89f662c576edce8fe50c68a
leoenterprise94 <42311754+leoenterprise94@users.noreply.github.com>
2019-05-17 13:09:36 -0600

    UserDomain y Data
    Cambios para la creación de un usuario con un fake Name y Avatar

--- Grupo6/ProyectoLenguajes/src/main/java/com/chat/bussines/UserBussines.java
+++ Grupo6/ProyectoLenguajes/src/main/java/com/chat/bussines/UserBussines.java
@@ -3,9 +3,12 @@ package com.chat.bussines;
 import java.util.ArrayList;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.chat.domain.User;
+
 public class UserBussines {
-	
-	private ArrayList<String> feelingsArrayList = new ArrayList<String>();
+
 	
 	private String names[] = {"Insecure","Nervous","Tense","Anxious","Angry","Confused","Weak","Petrified","Tense","Horrified",
 	"Inadequate","Flat","Motivade","Surprised","Satisfied","Upset","Miserable","Sad","Foolish","Discontented",
@@ -18,25 +21,27 @@ public class UserBussines {
 			"wild boar", "giraffe", "lion", "parrot", "fly", "mosquito", "bear", "sheep", " partridge", "dog",
 			"penguin", "chick", "grasshoppers", "snake", "tiger", "mole", "bull", "turtle", "cow", "fox" };
 	
-	public void createUsers() {
+	public void createUser() {
 		
 		Random r = new Random();
 		
 		int min=0;
 		int max=49;
 		
-		for (int i = 0; i < animals.length; i++) {
-		
 		int valueAnimal = r.nextInt((max - min) + 1) + min;
 		int valueNames  = r.nextInt((max - min) + 1) + min;
+		int valueAvatar  = r.nextInt((max - min) + 1) + min;
+		
+		
+		
 	
 		String userFake = names[valueNames]+" "+animals[valueAnimal];
+		String avatar= "/img/img"+valueAvatar;
 			
-			feelingsArrayList.add(userFake);
-		}
 		
-		feelingsArrayList.toString();
-		
+				
 	}
+	
+	
 
 }
