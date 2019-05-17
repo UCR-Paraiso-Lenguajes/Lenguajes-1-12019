d9da841af19fae5361842b336fcb0b34a0f11dfa
JeanCarlosMongeMendez <42584455+JeanCarlosMongeMendez@users.noreply.github.com>
2019-05-16 20:48:33 -0600

    laboratorio3
    B54524

---
+++ B54524/Orbis/src/main/java/com/orbis/B54524/ventas/controller/LibroController.java
@@ -0,0 +1,59 @@
+package com.orbis.B54524.ventas.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.orbis.B54524.ventas.business.EditorialBusiness;
+import com.orbis.B54524.ventas.business.LibroBusiness;
+import com.orbis.B54524.ventas.domain.Libro;
+import com.orbis.B54524.ventas.domain.form.LibroForm;
+
+@Controller
+public class LibroController {
+
+	@Autowired
+	private LibroBusiness libroBusiness;
+	@Autowired
+	private EditorialBusiness editorialBusiness;
+
+	@RequestMapping(value = "/libros", method = RequestMethod.GET)
+	public String listarLibros(Model model, @RequestParam("idLib") int editorialId) {
+		model.addAttribute("Libros", libroBusiness.obtenerLibrosPorEditorial(editorialId));
+		return "Libros";
+	}
+
+	@RequestMapping(value = "/insertarlibro", method = RequestMethod.GET)
+	public String insertarLibros(LibroForm libroForm, Model model) {
+		model.addAttribute("Editoriales", editorialBusiness.obtenerEditorial());
+		model.addAttribute("Autores", libroBusiness.obtenerAutores());
+		model.addAttribute("form", libroForm);
+		return "InsertarLibro";
+	}
+
+	@RequestMapping(value = "/insertarlibro", method = RequestMethod.POST)
+	public String guardarLibro(@Valid LibroForm libroForm, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors())
+			return insertarLibros(libroForm, model);
+		Libro libro = new Libro();
+		BeanUtils.copyProperties(libroForm, libro);
+		libro.getEditorial().setId(libroForm.getIdEditorial());
+		return libroBusiness.guardar(libro, libroForm.getIdAutores()) ? exitoInsertar(model) : errorInsertar(model);
+	}
+
+	@RequestMapping(value = "/errorinsertarlibro", method = RequestMethod.GET)
+	private String errorInsertar(Model model) {
+		return "ErrorInsertarLibro";
+	}
+
+	@RequestMapping(value = "/exitoinsertarlibro", method = RequestMethod.GET)
+	private String exitoInsertar(Model model) {
+		return "ExitoInsertarLibro";
+	}
+}
