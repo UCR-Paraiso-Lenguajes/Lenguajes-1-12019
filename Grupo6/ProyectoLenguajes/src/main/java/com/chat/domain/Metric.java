f8ea1ccdd8e84c6fbde6cfa51fc33ded7dd90471
valerialeiva <44216036+valerialeiva@users.noreply.github.com>
2019-05-16 18:45:47 -0600

    Validaciones dominio

--- Grupo6/ProyectoLenguajes/src/main/java/com/chat/domain/Metric.java
+++ Grupo6/ProyectoLenguajes/src/main/java/com/chat/domain/Metric.java
@@ -138,13 +138,4 @@ public class Metric {
 	}
 
 }
-package com.chat.domain;
 
-public class Metric {
-	private int quantityRoom;
-	private int quantityUser;
-	private double usersPerRoomProm;
-	private int longerMessage;
-	
-	
-}
--- Grupo6/ProyectoLenguajes/src/main/java/com/chat/domain/User.java
+++ Grupo6/ProyectoLenguajes/src/main/java/com/chat/domain/User.java
@@ -5,55 +5,18 @@ public class User {
 	private int id;
 	private String password;
 
+	public User() {
+		super();
+	}
+
 	public User(int id, String password) {
 		super();
 		if (id < 0)
 			throw new RuntimeException("El id del usuario debe ser mayor  0.");
 		if (password == null || password.trim().equals(""))
-			throw new RuntimeException("La contraseña es requerida es requerido");
+			throw new RuntimeException("La contraseï¿½a es requerida es requerido");
 		this.id = id;
 		this.password = password;
-	}
-
-	public User() {
-		super();
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", password=" + password + "]";
-	}
-
-}
-package com.chat.domain;
-
-public class User {
-
-	private int id;
-	private String password;
-
-	public User() {
-		super();
-	}
-
-	public User(int id, String password) {
-		super();
 		this.id = id;
 		this.password = password;
 	}
