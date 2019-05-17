6abe6e255ec95f785a48f57ccddf6a04c098fcc4
valerialeiva <44216036+valerialeiva@users.noreply.github.com>
2019-05-17 12:57:28 -0600

    Cambios domain user

--- Grupo6/ProyectoLenguajes/src/main/java/com/chat/domain/User.java
+++ Grupo6/ProyectoLenguajes/src/main/java/com/chat/domain/User.java
@@ -4,21 +4,28 @@ public class User {
 
 	private int id;
 	private String password;
+	private String avatar;
+	private String email;
 
 	public User() {
 		super();
 	}
 
-	public User(int id, String password) {
+	public User(int id, String password, String avatar, String email) {
 		super();
 		if (id < 0)
 			throw new RuntimeException("El id del usuario debe ser mayor  0.");
 		if (password == null || password.trim().equals(""))
-			throw new RuntimeException("La contrase�a es requerida es requerido");
+			throw new RuntimeException("La contrasena es requerida");
+		if (avatar == null || avatar.trim().equals(""))
+			throw new RuntimeException("El avatar requerido");
+		if (email == null || email.trim().equals(""))
+			throw new RuntimeException("El email requerido");
+
 		this.id = id;
 		this.password = password;
-		this.id = id;
-		this.password = password;
+		this.avatar = avatar;
+		this.email = email;
 	}
 
 	public int getId() {
@@ -37,8 +44,25 @@ public class User {
 		this.password = password;
 	}
 
-	
-	
-	
-	
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", password=" + password + ", avatar=" + avatar + ", email=" + email + "]";
+	}
+
 }
