bdc2a676042bdf8020a34b0d67f9c07685e3d795
Senju95 <josue.ad.95@gmail.com>
2019-05-17 18:59:42 -0600

    Cambiando accesos y  seguridad

--- Grupo1/DoveChat/src/main/java/com/dove/grupo1/msn/controller/RoomController.java
+++ Grupo1/DoveChat/src/main/java/com/dove/grupo1/msn/controller/RoomController.java
@@ -1,12 +1,14 @@
 package com.dove.grupo1.msn.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class RoomController {
 
 	@RequestMapping(value="/user/msn", method=RequestMethod.GET)
 	public String userLogin(){
-		return "login";
+		return "loginUser";
 	}
 }
