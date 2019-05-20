707ee2ca9ff38d88d2eb12830bbc7246ebd1561a
aaronMata <ajmspulg@gmail.com>
2019-05-20 16:42:39 -0600

    CommitAaron_Frontend
    Paginas html, controller solo para que funcione.
    Configuracion para cambio de idioma  .properties

---
+++ Grupo3/interactiveMessaging/src/main/java/com/projectOne/interactiveMessaging/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.projectOne.interactiveMessaging.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+    public String login() {
+        return "loginAdmi";
+    }
+	
+	@RequestMapping("/signIn")
+    public String signIn() {
+        return "loginUser";
+    }
+	
+	@RequestMapping("/chat")
+    public String chat() {
+        return "chat";
+    }
+	
+}
