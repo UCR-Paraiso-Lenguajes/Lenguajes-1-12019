d8b6c81489215fc3b19681706ab00f0a0b18364b
Manfred99 <manfredgh@hotmail.com>
2019-05-21 09:28:23 -0600

    Main Commit Manfred 21/5/2019

---
+++ Grupo3/interactiveMessaging/src/main/java/com/projectOne/interactiveMessaging/InteractiveMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.projectOne.interactiveMessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication//main
+public class InteractiveMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveMessagingApplication.class, args);
+	}
+
+}
