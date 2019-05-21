463d9dce1d5ecda5a742cd7aa0689e6f8dd15f07
Andres Coto <andres.coto3539@gmail.com>
2019-05-20 20:40:39 -0600

    pantalla crear chat, y allgunas class de cad paquete

---
+++ Grupo5/grupo5/src/main/java/com/proyectoUno/grupo5/controller/ChatController.java
@@ -0,0 +1,13 @@
+package com.proyectoUno.grupo5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ChatController {
+        @RequestMapping(value = "/crearChat", method = RequestMethod.GET)
+        public String crearChat(){
+        return "crearchat";
+        }
+}
---
+++ Grupo5/grupo5/src/main/java/com/proyectoUno/grupo5/dao/RoomDao.java
@@ -0,0 +1,4 @@
+package com.proyectoUno.grupo5.dao;
+
+public class RoomDao {
+}
