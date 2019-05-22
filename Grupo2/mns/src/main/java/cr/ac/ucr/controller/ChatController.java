859f89bbcde567e78960d9841d013eb9d6875549
yprim <priscipo@gmail.com>
2019-05-22 01:21:07 -0600

    se agrega la pantalla principal y otras vistas

--- Grupo2/mns/src/main/java/cr/ac/ucr/controller/ChatController.java
+++ Grupo2/mns/src/main/java/cr/ac/ucr/controller/ChatController.java
@@ -2,26 +2,18 @@ package cr.ac.ucr.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
-public class RoomController {
+public class ChatController {
 
 
-
-	@RequestMapping(value = "/createEmptyRoom", method = RequestMethod.GET)
+	@RequestMapping(value = "/chat", method = RequestMethod.GET)
 	public String showFormEmptyRoom(Model model)  {
-			return "createEmptyRoom";
-	}
-	
-	
-	@RequestMapping(value = "/createRoom", method = RequestMethod.GET)
-	public String showForm(Model model)  {
-			return "createFullRoom";
+			return "chat";
 	}
 	
 	
 }
-	
