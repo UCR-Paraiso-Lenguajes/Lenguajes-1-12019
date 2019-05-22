45fb16f627f318528f4ab4aa5a95c49799e22315
aaronMata <ajmspulg@gmail.com>
2019-05-22 15:32:54 -0600

    ThirdCommit_Frontend
    Cambios Html, properties, controller

--- Grupo3/interactiveMessaging/src/main/java/com/projectOne/interactiveMessaging/controller/HelloController.java
+++ Grupo3/interactiveMessaging/src/main/java/com/projectOne/interactiveMessaging/controller/HelloController.java
@@ -1,12 +1,20 @@
 package com.projectOne.interactiveMessaging.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.projectOne.interactiveMessaging.bussines.EmailBussines;
 
 @Controller
 public class HelloController {
 
+	@Autowired
+    private EmailBussines emailBussines;
+	
 	@RequestMapping("/")
     public String login() {
         return "loginAdmi";
@@ -14,6 +22,14 @@ public class HelloController {
 	
 	@RequestMapping("/signIn")
     public String signIn() {
+        return "loginUser";
+    }
+
+    @PostMapping("/signIn")
+    public String sendMail( @RequestParam("email") String mail){
+
+       emailBussines.sendMail("soporte.soft.inc@gmail.com",mail,"Invitacion","te invitamos por medio de este link:   ");
+
         return "loginUser";
     }
 	
