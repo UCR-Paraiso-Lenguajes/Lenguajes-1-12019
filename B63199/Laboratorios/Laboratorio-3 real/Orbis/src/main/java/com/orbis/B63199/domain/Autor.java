8076e1abba618b61cf9a695625e3546ac5a288df
lyka4406 <43503971+lyka4406@users.noreply.github.com>
2019-05-21 20:40:28 -0600

    l21519

---
+++ B63199/Laboratorios/Laboratorio-3 real/Orbis/src/main/java/com/orbis/B63199/domain/Autor.java
@@ -0,0 +1,70 @@
+package com.orbis.B63199.domain;
+
+import java.util.ArrayList;
+
+public class Autor {
+
+	private int id;
+	private String nombre;
+	private String apellidos;
+	private ArrayList<Libro> libros;
+
+	public Autor(int id, String nombre, String apellidos, ArrayList<Libro> libros) {
+		setId(id);
+		setNombre(nombre);
+		setApellidos(apellidos);
+		setLibros(libros);
+	}
+
+	public Autor() {
+		id = 0;
+		nombre = "";
+		apellidos = "";
+		libros = new ArrayList<>();
+	}
+
+	public boolean idEsValido(int id) {
+		return id < 0;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		if (idEsValido(id))
+			throw new RuntimeException("El id no puede ser negativo!");
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		if (nombre == null || nombre.trim().equals(""))
+			throw new RuntimeException("El nombre no es valido!");
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		if (apellidos == null || apellidos.trim().equals(""))
+			throw new RuntimeException("Los apellidos no son valido!");
+		this.apellidos = apellidos;
+	}
+
+	public ArrayList<Libro> getLibros() {
+		return libros;
+	}
+
+	public void setLibros(ArrayList<Libro> libros) {
+		if (libros == null)
+			throw new NullPointerException("La lista libros no puede ser nula!");
+		this.libros = libros;
+	}
+
+}
