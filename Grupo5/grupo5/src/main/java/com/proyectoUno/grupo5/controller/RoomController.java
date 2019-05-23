099e241a899a2fbc700199113f6ae122ce73abc1
Andres Coto <andres.coto3539@gmail.com>
2019-05-23 00:45:37 -0600

    user business

--- Grupo5/grupo5/src/main/java/com/proyectoUno/grupo5/controller/RoomController.java
+++ Grupo5/grupo5/src/main/java/com/proyectoUno/grupo5/controller/RoomController.java
@@ -1,6 +1,7 @@
 package com.proyectoUno.grupo5.controller;
 
 import com.proyectoUno.grupo5.business.RoomBussiness;
+import com.proyectoUno.grupo5.business.UserBussiness;
 import com.proyectoUno.grupo5.domain.Room;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,11 +16,12 @@ import java.sql.SQLOutput;
 public class RoomController {
     @Autowired
     private RoomBussiness room;
+
     @RequestMapping(value = "/insertRoom",method = RequestMethod.GET)
     public String insertRoom(){
         try {
             Room r = new Room("Pelusa",1);
-            if (room.insertRoom(r)) {
+            if (!room.insertRoom(r)) {
                 System.out.println("funciono");
             }else{
                 System.out.println("no funciono");
