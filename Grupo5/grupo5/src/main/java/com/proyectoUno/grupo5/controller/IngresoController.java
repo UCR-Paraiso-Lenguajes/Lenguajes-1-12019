c82b9430202118f1bf72aa5ad4d250b3d5d0aa75
Alfonsinius <43684079+Alfonsinius@users.noreply.github.com>
2019-05-23 02:41:19 -0600

    Registro por correo con la interfaz

--- Grupo5/grupo5/src/main/java/com/proyectoUno/grupo5/controller/IngresoController.java
+++ Grupo5/grupo5/src/main/java/com/proyectoUno/grupo5/controller/IngresoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,21 +20,28 @@ public class IngresoController {
 	
 	
     @RequestMapping(value = "/ingreso", method = RequestMethod.GET)
-    public String ingreso(Model model){
+    public String ingreso(Model model,@ModelAttribute(name="user") User user){
     	
     	
-    	User user = new User();
-    	user.setEmail("chatproyectouno@gmail.com");
-    	System.out.print("Entró");
+    	
+        return "ingreso";
+    }
+    @RequestMapping(value="/ingreso", method=RequestMethod.POST)
+    public String ingreso(@ModelAttribute(name="user") User user, Model model) {
+    	
+    	
+    	user.setEmail(user.getEmail());
+    	System.out.print("Correo enviado");
     	
     	try {
     		notificationService.sendNotification(user);
+    	
     	}catch(MailException e) {
     		//catch error
     	}
-        return "ingreso";
+    	
+    	return "correoExitoso";
     }
-    
     
     
     
