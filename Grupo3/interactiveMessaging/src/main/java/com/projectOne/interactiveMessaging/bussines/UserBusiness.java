4e08e2ad6e500f64311656e38b8af7196f3bcf9e
EmmanuelSolano <emmanuelsolano33@gmail.com>
2019-05-23 16:04:44 -0600

    controller y business data

---
+++ Grupo3/interactiveMessaging/src/main/java/com/projectOne/interactiveMessaging/bussines/UserBusiness.java
@@ -0,0 +1,21 @@
+package com.projectOne.interactiveMessaging.bussines;
+
+import java.util.Iterator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projectOne.interactiveMessaging.data.UserData;
+import com.projectOne.interactiveMessaging.domain.User;
+
+@Service
+public class UserBusiness {
+	
+	@Autowired
+	private UserData userData;
+	
+	public Iterator<User> findUsersOwnerOrAdmin(int idRoom, int idRole){
+		return userData.findUsersOwnerOrAdmin(idRoom, idRole);
+	}
+
+}
