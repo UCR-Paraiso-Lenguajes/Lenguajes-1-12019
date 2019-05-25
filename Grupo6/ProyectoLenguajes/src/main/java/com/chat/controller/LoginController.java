5f66a40060cf490b79b6bb1387cdae6447f602ea
lyka4406 <43503971+lyka4406@users.noreply.github.com>
2019-05-25 13:20:52 -0600

    Prueba conexión to J

--- Grupo6/ProyectoLenguajes/src/main/java/com/chat/controller/LoginController.java
+++ Grupo6/ProyectoLenguajes/src/main/java/com/chat/controller/LoginController.java
@@ -5,12 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.chat.data.MetricData;
+
 @Controller
 public class LoginController {
 
 	@RequestMapping(value="/login", method=RequestMethod.GET)
     public String login(Model model) 
 	{
+		MetricData metricData = new MetricData();
+		int cantidad=0;
+		cantidad = metricData.getQuantityRooms();
+		System.out.println(cantidad);
+		
 		return "login";
     }
 	
