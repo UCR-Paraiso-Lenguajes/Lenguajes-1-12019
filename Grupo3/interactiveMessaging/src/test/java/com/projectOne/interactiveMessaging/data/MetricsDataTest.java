cc70f2efa4a68ea06b58eddb4074731944941c5f
JesusTorresRodriguez <felipe-torres23@hotmail.com>
2019-05-25 19:27:19 -0600

    Test de Clases data

--- Grupo3/interactiveMessaging/src/test/java/com/projectOne/interactiveMessaging/data/MetricsDataTest.java
+++ Grupo3/interactiveMessaging/src/test/java/com/projectOne/interactiveMessaging/data/MetricsDataTest.java
@@ -1,13 +1,19 @@
 package com.projectOne.interactiveMessaging.data;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 
 import java.util.Iterator;
+import java.util.List;
+
 import static org.junit.Assert.assertTrue;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.projectOne.interactiveMessaging.domain.Metrics;
+import com.projectOne.interactiveMessaging.domain.Room;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,28 +30,104 @@ public class MetricsDataTest {
 	@Autowired
 	private MetricsData metricsData;
 	
-	@Test
-	public void saveMetrics() {
-		SimpleDateFormat dateformat = new SimpleDateFormat("yyyy/MM/dd");
-		try {
-			java.util.Date date = dateformat.parse("2019/04/10");
-			java.util.Date date2 =  dateformat.parse("2019/03/14");
-			assertTrue(metricsData.saveMetrics(5, 20, 30, 10, date, date2, 12, 100, 22, 9, 100, 400));
-			//metricsData.saveMetrics(3, 20, 30, 10, date, date2, 12, 100, 22, 9, 100, 400);
-		} catch (ParseException e) {
-			throw new RuntimeException("Error al declarar fecha");
-		}
-		
+	Iterator<Metrics> metrics;
+	
+	@Before
+    public void setUp() {
+    //Inicialización de variables antes de cada Test
+		metrics = metricsData.findMetrics(1);
 	}
 	
-	@Autowired
-	MetricsData metricsDao;
+//	@Test
+//	public void saveMetrics() {
+//		SimpleDateFormat dateformat = new SimpleDateFormat("yyyy/MM/dd");
+//		try {
+//			java.util.Date date = dateformat.parse("2019/04/10");
+//			java.util.Date date2 =  dateformat.parse("2019/03/14");
+//			assertTrue(metricsData.saveMetrics(5, 20, 30, 10, date, date2, 12, 100, 22, 9, 100, 400));
+//			//metricsData.saveMetrics(3, 20, 30, 10, date, date2, 12, 100, 22, 9, 100, 400);
+//		} catch (ParseException e) {
+//			throw new RuntimeException("Error al declarar fecha");
+//		}
+//		
+//	}
+	
+	
 	
 	@Test
-	public void findMetricsTest() {
-		Iterator<Metrics> metrics = metricsDao.findMetrics(1);
+	public void findMetrics_NotNull_Test() {
 		assertNotNull(metrics);
 	}
-
+	
+	@Test
+	public void findMetrics_IsEmpty_Test() {
+		assertTrue(metrics.hasNext());
+	}
+	
+	@Test
+	public void findMetrics_ObjectCorrect_Test() {
+		assertSame(metrics, metrics);
+	}
+	
+	@Test
+	public void findMetrics_forId_Test() {
+		int id= metrics.next().getId_Metrics();
+		assertEquals(1, id);
+	}
+	
+	@Test
+	public void findMetrics_numbersUsers_Test() {
+		int numbersUsers= metrics.next().getNumberOfUsers();
+		assertEquals(30, numbersUsers);
+	}
+	
+	@Test
+	public void findMetrics_getNumberOfRooms_Test() {
+		int numbersRooms= metrics.next().getNumberOfRooms();
+		assertEquals(5, numbersRooms);
+	}
+	
+	@Test
+	public void findMetrics_getAverageOfUsersPerRoom_Test() {
+		float average= metrics.next().getAverageOfUsersPerRoom();
+//		assertEquals(1.2, average);   ESTA NO FUNCIONA POR ALGUN MOTIVO 
+		assertEquals("1.2", average+"");
+	}
+	
+	@Test
+	public void findMetrics_getDateLastMessage_Test() {
+		Date date= (Date) metrics.next().getDateLastMessage();
+		assertEquals("2019-05-14", date+"");
+	}
+	
+	@Test
+	public void findMetrics_getDateFirstLogin_Test() {
+		Date date= (Date) metrics.next().getDateFirstLogin();
+		assertEquals("2019-05-19", date+"");
+	}
+	
+	@Test
+	public void findMetrics_getIdBigUser_Test() {
+		int idBigerUser= metrics.next().getIdBigUser();
+		assertEquals(21, idBigerUser);
+	}
+	
+	@Test
+	public void findMetrics_getNumberMessagesBigUser_Test() {
+		int numberMessageBIgUser= metrics.next().getNumberMessagesBigUser();
+		assertEquals(200, numberMessageBIgUser);
+	}
+	
+	@Test
+	public void findMetrics_getIdLongestMessage_Test() {
+		int idLongestMessage= metrics.next().getIdLongestMessage();
+		assertEquals(12, idLongestMessage);
+	}
+	
+	@Test
+	public void findMetrics_getIdLastRoomCreated_Test() {
+		int idLastRoomCreated= metrics.next().getIdLastRoomCreated();
+		assertEquals(22, idLastRoomCreated);
+	}
 
 }
--- Grupo3/interactiveMessaging/src/test/java/com/projectOne/interactiveMessaging/data/UserDataTest.java
+++ Grupo3/interactiveMessaging/src/test/java/com/projectOne/interactiveMessaging/data/UserDataTest.java
@@ -1,9 +1,13 @@
 package com.projectOne.interactiveMessaging.data;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Iterator;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,14 +23,56 @@ public class UserDataTest {
 	@Autowired
 	private UserData userData;
 	
+	Iterator<User>userList;
+	Iterator<Integer>idGroupsOfUsr;
+	@Before
+    public void setUp() {
+    //Inicialización de variables antes de cada Test
+		userList = userData.findUsersCertainRoom(2);
+		idGroupsOfUsr = userData.getIdGroupsOfUsr(1);
+	}
+	
 	@Test
-	public void findUsersTest() {
-		Iterator<User>userList = userData.findUsersCertainRoom(2);
+	public void findUsers_NotNull_Test() {
 		assertNotNull(userList);
 	}
+	
+	@Test
+	public void findUsers_ObjectCorrect_Test() {
+		assertSame(userList,userList);
+	}
+	
+	@Test
+	public void findUsers_isEmpty_Test() {
+		assertTrue(userList.hasNext());
+	}
+	
+	@Test
+	public void findUsers_idUser_Test() {
+		int id = userList.next().getUser_id();
+		assertEquals(1, id);
+	}
+	
+	@Test
+	public void findUsers_UserEmail_Test() {
+		String email = userList.next().getUser_email();
+		assertEquals("manfredgh@hotmail.com", email);
+	}
+	
+	@Test
+	public void findUsers_rolOne_Test() {
+		String role = userList.next().getRoles().get(1).getName_Role();
+		assertEquals("Owner", role);
+	}
+	
+	@Test
+	public void findUsers_rolTwo_Test() {
+		String namerole0 = userList.next().getRoles().get(0).getName_Role();
+		assertEquals("Admin", namerole0);
+	}
+	
 	@Test
 	public void getIdGroupsOfUsrTest() {
-		Iterator<Integer>idGroupsOfUsr = userData.getIdGroupsOfUsr(1);
 		assertNotNull(idGroupsOfUsr);
 	}
 }
