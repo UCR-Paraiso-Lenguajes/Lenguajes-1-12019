9777e7c8bd2e086dd9b2f4ef7fd6425df0cf7cab
SamSerra7 <43451776+SamSerra7@users.noreply.github.com>
2019-06-01 10:25:29 -0600

    Update Pelicula.java

--- B56860/Tareas/tarea4/Lab2/src/main/java/com/videocartago/renting/domain/Pelicula.java
+++ B56860/Tareas/tarea4/Lab2/src/main/java/com/videocartago/renting/domain/Pelicula.java
@@ -17,12 +17,12 @@ public abstract class Pelicula {
 	
 	public Pelicula(Genero genero, int codPelicula, int totalPeliculas, String titulo, boolean estreno,
 			boolean subtitulada) {
-		this.genero = genero;
-		this.codPelicula = codPelicula;
-		this.totalPeliculas = totalPeliculas;
-		this.titulo = titulo;
-		this.estreno = estreno;
-		this.subtitulada = subtitulada;
+		setGenero(genero);
+		setCodPeliula(codPelicula);
+		setTotalPeliculas(totalPeliculas);
+		setTitulo(titulo);
+		setEstreno(estreno);
+		setSubtitulada(subtitulada);
 	}
 	
 	protected abstract void setEstado() ;
@@ -31,36 +31,42 @@ public abstract class Pelicula {
 		return genero;
 	}
 	public void setGenero(Genero genero) {
+		if(genero == null) throw new RuntimeException("El genero no puede ser vacio");
 		this.genero = genero;
 	}
 	public int getCodPelicula() {
 		return codPelicula;
 	}
 	public void setCodPelicula(int codPelicula) {
+		if(codPelicula < 0) throw new RuntimeException("El código no puede ser negativo");
 		this.codPelicula = codPelicula;
 	}
 	public int getTotalPeliculas() {
 		return totalPeliculas;
 	}
 	public void setTotalPeliculas(int totalPeliculas) {
+		if(totalPeliculas < 0) throw new RuntimeException("El total de peliclas no puede ser negativo");
 		this.totalPeliculas = totalPeliculas;
 	}
 	public String getTitulo() {
 		return titulo;
 	}
 	public void setTitulo(String titulo) {
+		if(estreno) throw new RuntimeException("El titulo no puede ser vacio");
 		this.titulo = titulo;
 	}
 	public boolean isEstreno() {
 		return estreno;
 	}
 	public void setEstreno(boolean estreno) {
+		if(estreno == null) throw new RuntimeException("El estreno no puede ser vacio");
 		this.estreno = estreno;
 	}
 	public boolean isSubtitulada() {
 		return subtitulada;
 	}
 	public void setSubtitulada(boolean subtitulada) {
+		if(estreno == null) throw new RuntimeException("Debe escoger subtitulación");
 		this.subtitulada = subtitulada;
 	}
 
@@ -69,6 +75,7 @@ public abstract class Pelicula {
 	}
 	
 	public void setEstado(String estado) {
+		if(estado == null || estado.trim().equalsIgnoreCase("")) throw new RuntimeException("Debe escoger estado");
 		this.estado = estado;
 	}
 
@@ -77,6 +84,7 @@ public abstract class Pelicula {
 	}
 
 	public void setClasificacion(String clasificacion) {
+		if(clasificacion == null || clasificacion.trim().equalsIgnoreCase(""))  throw new RuntimeException("Debe escoger clasificación");
 		this.clasificacion = clasificacion;
 	}
 }
