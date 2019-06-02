98a23d0fb9fd8231974246c61ce1e31ca89c67be
leoenterprise94 <42311754+leoenterprise94@users.noreply.github.com>
2019-06-01 19:42:04 -0600

    Cambios Singleton
    Singleton Productora con los cambios solicitados

---
+++ B32915/videoRent/src/main/java/com/video/renting/domain/SingletonProductora.java
@@ -0,0 +1,48 @@
+package com.video.renting.domain;
+
+public class SingletonProductora {
+	
+	private Pelicula peli;
+	private Video video;
+	private Clip clip;
+	private static SingletonProductora singleton;
+	
+	private SingletonProductora(Pelicula peli, Video video, Clip clip) {
+		this.peli = peli;
+		this.video = video;
+		this.clip = clip;
+	}
+	
+	public static SingletonProductora getSingletonInstace(Pelicula peli, Video video, Clip clip) {
+		if(singleton == null) {
+			singleton = new SingletonProductora(peli,video,clip);
+		}
+		return singleton;
+	}
+
+	public Pelicula getPeli() {
+		return peli;
+	}
+
+	public void setPeli(Pelicula peli) {
+		this.peli = peli;
+	}
+
+	public Video getVideo() {
+		return video;
+	}
+
+	public void setVideo(Video video) {
+		this.video = video;
+	}
+
+	public Clip getClip() {
+		return clip;
+	}
+
+	public void setClip(Clip clip) {
+		this.clip = clip;
+	}
+	
+
+}
