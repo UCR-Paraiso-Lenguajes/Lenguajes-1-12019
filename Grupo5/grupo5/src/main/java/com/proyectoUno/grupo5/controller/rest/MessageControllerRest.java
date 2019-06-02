c37bfa462806c37524c59212ad6632fc67355fdb
Alfonsinius <43684079+Alfonsinius@users.noreply.github.com>
2019-06-02 13:59:37 -0600

    Chat

--- Grupo5/grupo5/src/main/java/com/proyectoUno/grupo5/controller/rest/MessageControllerRest.java
+++ Grupo5/grupo5/src/main/java/com/proyectoUno/grupo5/controller/rest/MessageControllerRest.java
@@ -1,11 +1,19 @@
 package com.proyectoUno.grupo5.controller.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.proyectoUno.grupo5.business.MessageBussiness;
+
 
 @RestController
 public class MessageControllerRest {
 
 	
+		@Autowired
+		private MessageBussiness messageBusiness;
+		
+		
 	
-	
+		
 }
