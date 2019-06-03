effed8119dee18a9b6d4b9307e44cd898658415d
danii230 <37677168+danii230@users.noreply.github.com>
2019-06-02 23:47:48 -0500

    busniss
    business

--- Grupo2/mns/src/main/java/cr/ac/ucr/business/RoomBusiness.java
+++ Grupo2/mns/src/main/java/cr/ac/ucr/business/RoomBusiness.java
@@ -1,16 +1,22 @@
 package cr.ac.ucr.business;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cr.ac.ucr.data.RoomData;
 import cr.ac.ucr.domain.Message;
 import cr.ac.ucr.domain.Room;
 @Service
 public class RoomBusiness {
-
+    
+	@Autowired
+	private RoomData roomData;
 	private Room room;
+	
 	
 	public Iterator<Message> listMessages(){
 		return room.getMessages();
@@ -34,8 +40,9 @@ public class RoomBusiness {
 		
 	}
 	
-	
-	
-	
+	public Room save(Room room) throws SQLException{
+	       return roomData.save(room);
+	        
+	}
 	
 }
