76d824f1ce68dc34cfe62f1a9814fd9fe56fef38
Andres Coto <andres.coto3539@gmail.com>
2019-06-04 09:12:34 -0600

    problemas de conflico

--- Grupo5/grupo5/src/main/java/com/proyectoUno/grupo5/controller/rest/MessageControllerRest.java
+++ Grupo5/grupo5/src/main/java/com/proyectoUno/grupo5/controller/rest/MessageControllerRest.java
@@ -25,40 +25,16 @@ public class MessageControllerRest {
 	@Autowired
 	private MessageBussiness messageBusiness;
 
+	private Map<Integer, Message> messagesSincro;
 
-		@Autowired
-		private MessageBussiness messageBusiness;
-		
-		 private Map<Integer, Message> messagesSincro;
-		
+
 		@RequestMapping(value = "/insertMessage", method = RequestMethod.POST)
-		public ResponseEntity<Object> postInsertMessages(@RequestBody Message message) {
-				
-			return null;
-			//messagesSincro = messageBusiness.insertMessageSincro(message);
-		}
-		public ResponseEntity<Object> getFoosBySimplePathWithPathVariable (@RequestBody Message message) {
-
-			//Message message = new Message(containt, idUser, idRoom);
-			return new ResponseEntity<>(messageBusiness.insertMessage(message), HttpStatus.OK);
-}
-		
-		@RequestMapping(value = "/getMessageSincro", method = RequestMethod.GET)
-		//public ResponseEntity<Object> getMessages(@RequestBody int IdRoom){
-		public Map<Integer, Message> getMessages(){
-			
-			
-			//return new ResponseEntity<>(messageBusiness.getMessagesSincro(1), HttpStatus.OK);
-			return messagesSincro;
-		}
-
-	@RequestMapping(value = "/msn/insertMessage", method = RequestMethod.POST)
 	public ResponseEntity<Object> postInsertMessages(@RequestBody Message message) {
 
 		return new ResponseEntity<>(messageBusiness.insertMessage(message), HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/msn/getMessages", method = RequestMethod.GET)
+		@RequestMapping(value = "/getMessages", method = RequestMethod.GET)
 	public ResponseEntity<Object> getMessages(){
 		return new ResponseEntity<>(messageBusiness.getMessagesByIdRoom(2),  HttpStatus.OK);
 	}
