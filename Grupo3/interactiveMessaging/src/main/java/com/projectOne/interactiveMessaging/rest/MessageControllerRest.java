d9deed710997e1d7fcd75a55495c3604a6b418cb
Manfred99 <manfredgh@hotmail.com>
2019-06-04 16:24:31 -0600

    Commit

--- Grupo3/interactiveMessaging/src/main/java/com/projectOne/interactiveMessaging/rest/MessageControllerRest.java
+++ Grupo3/interactiveMessaging/src/main/java/com/projectOne/interactiveMessaging/rest/MessageControllerRest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.projectOne.interactiveMessaging.bussines.GroupBusiness;
 import com.projectOne.interactiveMessaging.bussines.MessageBusiness;
+import com.projectOne.interactiveMessaging.bussines.UserBusiness;
 import com.projectOne.interactiveMessaging.data.GroupData;
 import com.projectOne.interactiveMessaging.data.UserData;
 import com.projectOne.interactiveMessaging.domain.ClassListNewMessages;
@@ -22,6 +24,7 @@ import com.projectOne.interactiveMessaging.domain.Message;
 import com.projectOne.interactiveMessaging.domain.MessageToSend;
 import com.projectOne.interactiveMessaging.domain.SingleGroupMessage;
 import com.projectOne.interactiveMessaging.domain.TableMessagesGroups;
+import com.projectOne.interactiveMessaging.domain.User;
 
 @RestController
 public class MessageControllerRest {
@@ -31,6 +34,8 @@ public class MessageControllerRest {
 	private UserData userData;
 	@Autowired
 	private GroupBusiness groupBusiness;
+	@Autowired
+	private UserBusiness userBusiness;
 	@RequestMapping(value="/api/messages", method=RequestMethod.GET)
 	public @ResponseBody ArrayList<Message> listMessages(@RequestParam(value="idGroup") int idGroup,
 			@RequestParam(value="idUserRoom") int idUserRoom){
@@ -41,6 +46,14 @@ public class MessageControllerRest {
 				messageBusiness.getMessagesByRange(1, 999999, userData.findUsersCertainRoom(idGroup),groupBusiness.getNameGroupTableMessages(idGroup),idUserRoom);
 		return messages;
 	}
+
+	@RequestMapping(value="/api/groupInfo", method=RequestMethod.GET)
+    public @ResponseBody ArrayList<User> getUsersGroup(Model model, @RequestParam("idGroup") int idGroup) {
+		Iterator<User> users = userBusiness.findUsersCertainRoom(idGroup);
+		ArrayList<User> usersA = new ArrayList<>();
+		users.forEachRemaining(usersA::add);
+        return usersA;
+    }
 	@RequestMapping(value="/api/sendMessages/{idGroup}/{idUserRoom}", method=RequestMethod.PUT)
 	public @ResponseBody void sendMessage(@PathVariable int idGroup,
 			@PathVariable int idUserRoom,
