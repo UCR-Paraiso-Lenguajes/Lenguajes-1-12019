197769c3c6529470e92874d46d60b1715f457d90
aaronMata <ajmspulg@gmail.com>
2019-06-04 02:39:09 -0600

    Update_Metrics

--- Grupo3/interactiveMessaging/src/main/java/com/projectOne/interactiveMessaging/controller/MetricsController.java
+++ Grupo3/interactiveMessaging/src/main/java/com/projectOne/interactiveMessaging/controller/MetricsController.java
@@ -1,7 +1,5 @@
 package com.projectOne.interactiveMessaging.controller;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 
-import com.projectOne.interactiveMessaging.bussines.EmailBussines;
 import com.projectOne.interactiveMessaging.bussines.GroupBusiness;
-import com.projectOne.interactiveMessaging.bussines.MessageBusiness;
 import com.projectOne.interactiveMessaging.bussines.MetricsBusiness;
-import com.projectOne.interactiveMessaging.bussines.UserBusiness;
-import com.projectOne.interactiveMessaging.data.UserData;
-import com.projectOne.interactiveMessaging.domain.Message;
 import com.projectOne.interactiveMessaging.domain.Metrics;
 import com.projectOne.interactiveMessaging.domain.Room;
 
@@ -24,12 +17,6 @@ import com.projectOne.interactiveMessaging.domain.Room;
 @Controller
 public class MetricsController {
 
-
-	@Autowired
-	private MessageBusiness messageBusiness;
-
-	@Autowired
-	private UserData userData;
 	@Autowired
 	private GroupBusiness groupBusiness;
 	
@@ -44,10 +31,6 @@ public class MetricsController {
 //		return "metrics";
 		Metrics metrics = metricsBusiness.recoverMetricsData();
 
-		ArrayList<Message> messages=
-
-				messageBusiness.getMessagesByRange(1, 999999, userData.findUsersCertainRoom(2),"LosMagnificosMessages",1);///Aqui cambiar el usuario y grupo
-						model.addAttribute("messages",messages);
 						model.addAttribute("idUserRoom",1);///Aqui cambiar el usuario
 						List<Room> groups = groupBusiness.getGroupsOfUser(1);///Aqui cambiar el id usuario
 						model.addAttribute("groups",groups);
