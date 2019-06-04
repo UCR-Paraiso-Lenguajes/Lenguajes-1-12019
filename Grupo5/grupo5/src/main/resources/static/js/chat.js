b9dda7cad60a17de66f24325e6cb7cbcebb3a414
Andres Coto <andres.coto3539@gmail.com>
2019-06-03 23:00:06 -0600

    ya se pueden ver los rooms de cada usuario

--- Grupo5/grupo5/src/main/resources/static/js/chat.js
+++ Grupo5/grupo5/src/main/resources/static/js/chat.js
@@ -5,11 +5,17 @@ var room = new Vue({
         isRoom: false,
         message: '',
         messages: [],
-        rooms: []
+        rooms: [],
+        idUser: '',
+        idRoom: '',
+        sendMessage: []
     },
     mounted() {
+        let url = 'url'+window.location;
+        let user = url.split('=');
+        this.idUser = user[1];
         axios
-            .get('http://localhost:8080/api/rooms')
+            .get('http://localhost:8080/getRoomPerUser?idUser='+this.idUser)
             .then(response => (this.rooms = response.data))
     },
     methods: {
@@ -17,12 +23,16 @@ var room = new Vue({
             this.isRoom = !this.isRoom;
             this.isChat = false;
         },
-        verChatPerRoom: function () {
+        verChatPerRoom: function (idRoom) {
+            this.idRoom = idRoom;
             this.isChat = !this.isChat;
             this.isRoom = false;
+            alert(this.idRoom);
         },
         enviarMensaje: function () {
             this.messages.push({
+                idUser: this.idUser,
+                idRoom: this.idRoom,
                 message: this.message
             }),
                 this.message = '';
