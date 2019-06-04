9a33bb2fb1050b3cba54ee3113ea5bdc222f94cd
Esteban Barrantes Mora <39637041+EstebanBarrantesMora95@users.noreply.github.com>
2019-06-04 15:11:35 -0600

    Update RoomDao y UT de delete User

---
+++ Grupo5/grupo5/src/test/java/com/proyectoUno/grupo5/MetricsBusines.java
@@ -0,0 +1,59 @@
+package com.proyectoUno.grupo5;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.proyectoUno.grupo5.business.MetricsBusiness;
+import com.proyectoUno.grupo5.business.RoomBussiness;
+import com.proyectoUno.grupo5.business.UserBusiness;
+import com.proyectoUno.grupo5.domain.Room;
+import com.proyectoUno.grupo5.domain.User;
+
+public class MetricsBusines {
+
+	@Autowired
+	private MetricsBusiness metricsBusiness;
+	
+
+	@Autowired
+	private UserBusiness userBusiness;
+	
+	@Autowired
+	private RoomBussiness roomBusiness;
+	
+	@Test
+	public void test() {
+		try
+		{
+			User user = new User();
+			user.setIdUser(25);
+			user.setEmail("esteban5678@gmail.com");
+			userBusiness.insertUser(user);
+			Room room = new Room();
+			room.setIdRoom(25);
+			room.setRoomName("test");
+			try {
+				roomBusiness.insertRoom(room);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			roomBusiness.assignRoom(user.getIdUser());
+			
+			
+			
+			
+			metricsBusiness.delete(room.getIdRoom(), user.getIdUser());
+			assertTrue( false);
+		}
+		catch (RuntimeException e) 
+		{
+			assertTrue(  true);
+		}
+	}
+
+}
