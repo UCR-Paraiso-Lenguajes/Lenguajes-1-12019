b5ac83528da5daaf2c23e9a0d16e3def3aed9f25
Manfred99 <manfredgh@hotmail.com>
2019-06-03 18:25:34 -0600

    ArregloCommit

---
+++ Grupo3/interactiveMessaging/target/classes/static/js/myWorker.js
@@ -0,0 +1,92 @@
+function startTimer() {
+    var start = Date.now(),
+        diff,
+        seconds;
+    function timer() {
+        // get the number of seconds that have elapsed since 
+        // startTimer() was called
+        diff = 1 - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        
+        seconds = (diff % 60) | 0;
+
+       
+        //seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        
+        if(diff===0){
+        	//console.log(0);
+        	upDateNEWMessagesFrame();
+        }
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
+        }
+    };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+    interv = setInterval(timer, 1000);
+}
+function upDateMessagesFrame(){
+	ulM.empty();
+	$
+	.getJSON(
+			"http://"+host+":8080/msn/api/messages/?idGroup="
+					+ idGroup.value + "&idUserRoom="
+					+ idUserRoom.val(),
+			{
+				name : 'messages'
+			},
+			function(data, textStatus, jqXHR) {
+
+				var msclass;
+				var mstext;
+
+				var size = 0;
+				for (key in data) {
+					if (data.hasOwnProperty(key))
+						msclass = data[size].type;
+					mstext = data[size].message;
+					$(
+							'<li class='+msclass+'><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>'
+									+ mstext + '</p></li>')
+							.appendTo($('.messages ul'));
+					size++;
+				}
+				;
+
+			});
+}
+function upDateNEWMessagesFrame(){
+	$
+	.getJSON(
+			"http://"+host+":8080/msn/api/newmessages/?idGroup="
+					+ idGroup.value + "&idUserRoom="
+					+ idUserRoom.val(),
+			{
+				name : 'messages'
+			},
+			function(data, textStatus, jqXHR) {
+
+				var msclass;
+				var mstext;
+
+				var size = 0;
+				for (key in data) {
+					if (data.hasOwnProperty(key))
+						msclass = data[size].type;
+					mstext = data[size].message.message;
+					$(
+							'<li class='+msclass+'><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>'
+									+ mstext + '</p></li>')
+							.appendTo($('.messages ul'));
+					size++;
+				}
+				;
+
+			});
+}
+
