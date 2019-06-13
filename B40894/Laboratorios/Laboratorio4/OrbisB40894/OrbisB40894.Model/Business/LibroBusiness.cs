c17bf509430a8004811bf32aa944e958ef145b59
Esteban Barrantes Mora <39637041+EstebanBarrantesMora95@users.noreply.github.com>
2019-06-13 16:05:48 -0600

    laboratorio4

---
+++ B40894/Laboratorios/Laboratorio4/OrbisB40894/OrbisB40894.Model/Business/LibroBusiness.cs
@@ -0,0 +1,26 @@
+﻿using OrbisB40894.Model.Data;
+using OrbisB40894.Model.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrbisB40894.Model.Business
+{
+   public class LibroBusiness
+    {
+        private string connection;
+        private LibroData libroData;
+        public LibroBusiness(string connection)
+        {
+            if (string.IsNullOrEmpty(connection)) throw new Exception("El string de conexión es requerido.");
+
+            this.connection = connection;
+            this.libroData = new LibroData();
+        }
+
+        public IEnumerable<Libro> List(int id_publicador)
+        {
+            return this.libroData.GetByCodPublicador(id_publicador);
+        }
+    }
+}
