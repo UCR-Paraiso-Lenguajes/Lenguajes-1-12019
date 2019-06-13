85532b3cba70f300f02419ea6d8213d08e9e696b
Alfonsinius <43684079+Alfonsinius@users.noreply.github.com>
2019-06-13 15:53:05 -0600

    LaboratorioUnoB63685

---
+++ B63685/Labs/LabUnoASPNET/OrbisB63685Model/Business/EditorialBusiness.cs
@@ -0,0 +1,22 @@
+﻿using OrbisB63685Model.Data;
+using OrbisB63685Model.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrbisB63685Model.Business
+{
+    public class EditorialBusiness
+    {
+        EditorialData editorialData = new EditorialData();
+
+        public IEnumerable<Editorial> GetAll()
+        {
+
+            return editorialData.GetAll();
+        }
+
+
+    }
+}
+
---
+++ B63685/Labs/LabUnoASPNET/OrbisB63685Model/Domain/Libro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrbisB63685Model.Domain
+{
+    public class Libro
+    {
+        private int codLibro;
+        private string tituloLibro;
+        private string isbn;
+        private int anoPublicacion;
+        private int idEditorial;
+        private List<Autor> autores;
+        private double precio;
+
+        public Libro(int codLibro, string tituloLibro, double precio)
+        {
+            this.codLibro = codLibro;
+            this.tituloLibro = tituloLibro;
+            this.precio = precio;
+        }
+
+        public int CodLibro { get => codLibro; set => codLibro = value; }
+        public string TituloLibro { get => tituloLibro; set => tituloLibro = value; }
+        public string Isbn { get => isbn; set => isbn = value; }
+        public int AnoPublicacion { get => anoPublicacion; set => anoPublicacion = value; }
+        public int IdEditorial { get => idEditorial; set => idEditorial = value; }
+        public double Precio { get => precio; set => precio = value; }
+        internal List<Autor> Autores { get => autores; set => autores = value; }
+    }
+}
