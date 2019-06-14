c7d6294b7f255ddbe8c5f48e595414dd4b9664c7
Daniel fonseca Solano <31803434+dfs11@users.noreply.github.com>
2019-06-13 18:05:29 -0600

    Lab4-b62734

---
+++ B62734/lab4/Orbis[B62734].Model/Domain/Autor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orbis_B62734_.Model.Domain
+{
+    class Autor
+    {
+
+        public string nombre_autor{ get; set; }
+        public string apellidos { get; set; }
+        
+
+    }
+}
