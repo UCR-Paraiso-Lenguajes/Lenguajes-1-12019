ba7faf97a2da0b3835187007b7cb1f60a545af93
danii230 <37677168+danii230@users.noreply.github.com>
2019-06-13 20:27:35 -0600

    Laboratorio 4 Lyka

---
+++ B63199/Laboratorios/Laboratorio4/Lab1ASPdotNet/Orbis[B63199].Model/Domain/Publicador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Model.Domain
+{
+    public class Publicador
+    {
+        public int idPublicador {internal set; get; }
+        public string nombrePubicador { internal set; get; }
+        public string urlSitioWeb { internal set; get; }
+
+        public Publicador(int idPublicador, string nombrePubicador, string urlSitioWeb)
+        {
+            this.idPublicador = idPublicador;
+            this.nombrePubicador = nombrePubicador;
+            this.urlSitioWeb = urlSitioWeb;
+        }
+    }
+}
