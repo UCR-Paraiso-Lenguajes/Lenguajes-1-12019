41f676532baa2b6ad1e533126233410e50f2adbb
leoenterprise94 <42311754+leoenterprise94@users.noreply.github.com>
2019-06-13 18:34:20 -0600

    Lab4 B32915
    Lab4 B32915

---
+++ B32915/OrbisB32915.Web/OrbisB32915.Web/Controllers/LibroController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using OrbisB32915.Web.Business;
+using OrbisB32915.Web.Data;
+using OrbisB32915.Web.Domain;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrbisB32915.Web.Controllers
+{
+    public class LibroController : Controller
+    {
+        readonly IConfiguration configuration;
+        public LibroController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        
+        private LibroBusiness libroBusiness = new LibroBusiness();
+        private EditorialBusiness editorialBusiness = new EditorialBusiness();
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Libro> librosPorPublicador = libroBusiness.GetByPublicadorId();
+            IEnumerable<Editorial> listaEditoriales = editorialBusiness.GetAll();
+
+            dynamic Model = new ExpandoObject();
+
+            Model.Editoriales = listaEditoriales;
+            Model.Libros = librosPorPublicador;
+
+            return View(Model);
+        }
+        [HttpPost]
+        public IActionResult Index(int codEditorial)
+        {
+            IEnumerable<Libro> librosPorPublicador = libroBusiness.GetByPublicadorId(codEditorial);
+
+            return View(librosPorPublicador);
+        }
+    }
+
+}
