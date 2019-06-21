c3d7897a808193103f032f3c6571b41b90d77da9
Daniel fonseca Solano <31803434+dfs11@users.noreply.github.com>
2019-06-21 00:14:15 -0600

    Proyecto creado

---
+++ Proyecto2-Grupo7/Proyecto-2/Proyecto2.Clases/Domain/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto2.Clases.Domain
+{
+   public class Pedido
+    {
+        Carrito OrdendeCompra;
+        string Id;
+        string Email;
+        string Direccion;
+        List<ProductoCantidad> productos;
+
+    }
+}
