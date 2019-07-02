4c71f2321d97385efcdf4d68aed4f1f439e0dfde
Esteban Barrantes Mora <39637041+EstebanBarrantesMora95@users.noreply.github.com>
2019-07-01 21:40:31 -0600

    AgregadoDeProyectos

---
+++ Grupo5/ProyectoDosGrupoCinco/ProyectoDosGrupoCinco/Data/CarritoData.cs
@@ -0,0 +1,42 @@
+﻿using ProyectoDosGrupoCinco.Domain;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace ProyectoDosGrupoCinco.Data
+{
+    public class CarritoData
+    {
+
+        public IEnumerable<Carrito> List()
+        {
+            List<Carrito> carritos = new List<Carrito>();
+            using (SqlConnection connection = new SqlConnection("data source=" +
+                "163.178.173.148;initial " +
+                "catalog=NombreDeLaBase;user id=estudiantesrp;password=estudiantesrp;" +
+                "multipleactiveresultsets=True"))
+            {
+                connection.Open();
+                string sql = "select * from NombreDeLaTabla";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string nombre = reader.GetString(1);
+
+                            
+                          //  carritos.Add(new CarritoDisponible(id, nombre));
+                        }
+                        reader.Close();
+                    };
+                }
+                connection.Close();
+            }
+
+            return carritos;
+        }
+
+    }
+}
