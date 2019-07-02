e75a0505c6869bf8bbfc2cd5c57db4f9c6492931
yprim <priscipo@gmail.com>
2019-07-02 17:46:49 -0600

    se actualiza la parte de ract

--- B24075/examen2/partereact/examen/src/apiRequests.js
+++ B24075/examen2/partereact/examen/src/apiRequests.js
@@ -1,10 +1,12 @@
 
 // Consulta la lista de departamentos
 async function getDivisions() {
-    // Por el momento retorna los datos quemados, el api debe devolver un JSON con este formato
-    // Borrar cuando el api este implementada  
-    // Descomentar cuando el api este implementada
-     let response = await fetch('https://localhost:44369/api/departamento/');
+
+    var myHeaders = new Headers();
+    myHeaders.append("Access-Control-Allow-Origin", "*");
+    var headers = { headers: myHeaders };
+
+     let response = await fetch('https://localhost:44369/api/departamento/',headers);
      let json = await response.json();
      console.log(json);
      return json;
