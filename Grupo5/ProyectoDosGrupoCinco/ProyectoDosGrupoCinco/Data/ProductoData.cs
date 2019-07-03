17f796378f4ec8979b3ab7fcf406dacd7bd2864d
Esteban Barrantes Mora <39637041+EstebanBarrantesMora95@users.noreply.github.com>
2019-07-02 21:29:58 -0600

    Create ProductoData.cs

---
+++ Grupo5/ProyectoDosGrupoCinco/ProyectoDosGrupoCinco/Data/ProductoData.cs
@@ -0,0 +1,42 @@
+﻿using ProyectoDosGrupoCinco.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ProyectoDosGrupoCinco.Data
+{
+    public class ProductoData
+    {
+        public IEnumerable<Producto> ListProducts()
+        {
+            List<Producto> productos = new List<Producto>();
+            using (SqlConnection connection = new SqlConnection("data source=" +
+                "163.178.173.148;initial " +
+                "catalog=NombreDeLaBase;user id=estudiantesrp;password=estudiantesrp;" +
+                "multipleactiveresultsets=True"))
+            {
+                connection.Open();
+                string sql = "select * from NombreDeLaTabla";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string nombre = reader.GetString(1);
+
+
+                            //  carritos.Add(new CarritoDisponible(id, nombre));
+                        }
+                        reader.Close();
+                    };
+                }
+                connection.Close();
+            }
+
+            return productos;
+        }
+    }
+}
