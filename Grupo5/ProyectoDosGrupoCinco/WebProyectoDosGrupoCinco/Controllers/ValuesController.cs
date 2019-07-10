268923a85a841382f5a241270e648ff91283fdd0
Alfonsinius <43684079+Alfonsinius@users.noreply.github.com>
2019-07-10 16:03:41 -0600

    update correo

--- Grupo5/ProyectoDosGrupoCinco/WebProyectoDosGrupoCinco/Controllers/ValuesController.cs
+++ Grupo5/ProyectoDosGrupoCinco/WebProyectoDosGrupoCinco/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace WebProyectoDosGrupoCinco.Controllers
         public ActionResult<IEnumerable<string>> Get()
         {
 
-            ordenDeCompraBusiness.resumenCompra("alfonsoaju6@gmail.com", "hola");
+            ordenDeCompraBusiness.resumenCompra("alfonsoaju6@gmail.com", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
     
             
 
