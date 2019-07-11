a1a07a46d7c0c1a2148dac75263c06ae45ae4eda
Esteban Barrantes Mora <39637041+EstebanBarrantesMora95@users.noreply.github.com>
2019-07-10 22:29:21 -0600

    UnitTest

--- Grupo5/ProyectoDosGrupoCinco/TestProyectoDosGrupoCinco/UnitTest1.cs
+++ Grupo5/ProyectoDosGrupoCinco/TestProyectoDosGrupoCinco/UnitTest1.cs
@@ -1,4 +1,9 @@
 using NUnit.Framework;
+using ProyectoDosGrupoCinco.Business;
+using ProyectoDosGrupoCinco.Data;
+using ProyectoDosGrupoCinco.Domain;
+using System;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -14,5 +19,90 @@ namespace Tests
         {
             Assert.Pass();
         }
+
+
+        [Test]
+        public void BuscarProducto()
+        {
+            ProductoData productoData = new ProductoData();
+
+            Producto producto=productoData.GetProductById(3);
+
+
+            Assert.AreEqual("llavero", producto.Nombre);
+            Assert.AreEqual(3, producto.IdProducto);
+            Assert.AreEqual(5.00, producto.Impuesto);
+            Assert.AreEqual(500, producto.Precio);
+            Assert.AreEqual("Llavero de game of thrones", producto.Descripcion);
+            Assert.AreEqual(1000, producto.CantidadDisponible);
+  
+
+        }
+
+        [Test]
+        public void BuscarProducto2()
+        {
+            decimal impuesto = 5;
+            Producto nuevoProducto=new Producto(impuesto, "test", "test", 50, 50);
+
+            
+            
+            ProductoData productoData = new ProductoData();
+
+            productoData.InsertarProducto(nuevoProducto);
+
+            List<Producto> productos = productoData.ListProducts() ;
+
+            Producto ultimoProductoLista = new Producto();
+
+            foreach (Producto item in productos)
+            {
+
+                ultimoProductoLista = item;
+
+            }
+
+            Producto producto = productoData.GetProductById(ultimoProductoLista.IdProducto);
+
+            
+
+            Assert.AreEqual("test", producto.Nombre);
+            Assert.AreEqual(ultimoProductoLista.IdProducto, producto.IdProducto);
+            Assert.AreEqual(impuesto, producto.Impuesto);
+            Assert.AreEqual(50, producto.Precio);
+            Assert.AreEqual("test", producto.Descripcion);
+            Assert.AreEqual(50, producto.CantidadDisponible);
+
+
+        }
+
+        [Test]
+        public void ListarProducto()
+        {
+              ProductoBusiness productoBusiness = new ProductoBusiness();
+            List<Producto> productos = productoBusiness.CargarProductos(2);
+            
+             bool verifica = false;
+
+              foreach (Producto item in productos)
+              {
+
+                  if (item.IdProducto > 50) {
+                      verifica = true;
+                    Console.WriteLine(item.IdProducto);
+                }
+
+              }
+
+            
+
+            Assert.AreEqual(verifica, true);
+
+
+
+
+        }
+
+
     }
 }
