c29cfe1f024cc4d71111f2290257ecdef7dc0ea2
Manfred99 <manfredgh@hotmail.com>
2019-07-11 02:45:22 -0600

    ManfredCommit

--- GrupoMSEProyecto2/React2/Proyecto2.Model/Data/CarritoData.cs
+++ GrupoMSEProyecto2/React2/Proyecto2.Model/Data/CarritoData.cs
@@ -12,37 +12,82 @@ namespace Proyecto2.Model.Data
     {
         private string connectionString;
 
-        string sqlconnect = "data source = " +
+        private string sqlconnect = "data source = " +
                 "163.178.173.148;initial " +
                 "catalog=IF4101_2019_PROYECTO2;user id=estudiantesrp;password=estudiantesrp;" +
                 "multipleactiveresultsets=True";
 
-        public CarritoData(string connectiostring)
+        public CarritoData()
         {
-            this.connectionString = connectiostring;
+            this.connectionString = sqlconnect;
         }
 
-        public IEnumerable<Carrito> ObtenerCarritoPorUsuario(int idComprador,String email)
+        public Object ObtenerCarritoPorUsuario(int idComprador)
         {
-            List<Carrito> carrito = new List<Carrito>();
+            Carrito carrito;
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                string sql = "select c.id, u.id, u.nombre, u.passwordComprador, u.emailPrincipal, "+
-"p.id, p.impuesto, p.nombre, p.descripcion, p.precio, p.cantidadDisponible from Carrito c inner join Comprador u on c.idUser = u.id "+
-"inner join CarritoProducto cp on cp.idCarrito = c.id inner join Producto p on cp.idProducto = p.id where u.emailPrincipal = "+email;
+                string sql = "select c.id as idCarrito, u.id as userId, u.nombre, u.passwordComprador, u.emailPrincipal, " +
+                             "cp.idPedido as pedidoId, p.email as pedidoEmail, p.direccion as pedidoDireccion, " +
+                             "p.tipo as pedidoTipo, p.fechaUltimoUso as pedidoFechaUltUso, p.fechaEntrega as pedidoFechaEntrega, " +
+                             "p.totalProductosEntregados as pedidoTotalProdEntreg, p.fechaDespacho as pedidoFechaDespacho " +
+                             "from  Comprador u inner join Carrito c on  u.id = c.idUser " +
+                             "inner join CarritoPedido cp on cp.idCarrito = c.id inner join Pedido p on cp.idPedido = p.id " +
+                             "where u.id = " + idComprador;
                 using (SqlCommand command = new SqlCommand(sql, connection))
                 {
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            string id = reader.GetString(0);
-                            int idUser = reader.GetInt32(1);
+                            /*idCarrito,userId,nombre,passwordComprador,emailPrincipal,pedidoId*/
+                            string idCarrito = reader.GetString(0);
 
-                            //Carrito(string id, Usuario usuario, decimal subtotal, int total, List < ProductoCantidad > productoCantidad)
-                            //ProductoCantidad(Producto producto, int cantidad, float compraTotalProducto)
-                            //carrito.Add(new Carrito(id_publicador, nombre_publicador, url_sitio_web));
+                            int userId = reader.GetInt32(1);
+                            string userNombre = reader.GetString(2); 
+                            string passwordUser = reader.GetString(3); 
+                            string userEmail = reader.GetString(4);
+
+                            int pedidoId = reader.GetInt32(5);
+                            string emailPedido = reader.GetString(6);
+                            string direccionPedido = reader.GetString(7);
+                            string tipoPedido = reader.GetString(8);
+                            DateTime pedidoFechaUltUso = reader.GetDateTime(9);
+                            DateTime pedidoFechaEntrega = reader.GetDateTime(10);
+                            int pedidoTotalProdEntreg = reader.GetInt32(11);
+                            DateTime pedidoFechaDespacho = reader.GetDateTime(12);
+
+
+                            //Procedemos a obtener la lista de productoCantidad
+                            List<ProductoCantidad> productosCantidad = getProductosCarritoIdPedido(pedidoId);
+                            OrdenDeCompra ordenDeCompra = new OrdenDeCompra(productosCantidad);
+                            int productosTotal = 0;
+                            foreach (ProductoCantidad productoCantidad in productosCantidad)
+                            {
+                                productosTotal = productosTotal + productoCantidad.Cantidad;
+                            }
+                            Pedido pedido;
+                            if (tipoPedido.Equals("Entregado"))
+                            {
+                                pedido = new PedidoEntregado(pedidoId, emailPedido, direccionPedido, ordenDeCompra,
+                                    pedidoFechaEntrega,ordenDeCompra.TotalCompra, productosTotal);
+                                carrito = new Carrito(idCarrito, new Usuario(userId, userNombre, passwordUser, userEmail), pedido);
+                                return carrito;
+                            }else if (tipoPedido.Equals("Despachado"))
+                            {
+                                //int id, string email, string direccion, OrdenDeCompra ordenDeCompra, DateTime fechaDespacho, double totalCompra
+                                pedido = new PedidoDespachado(pedidoId, emailPedido, direccionPedido, ordenDeCompra,
+                                    pedidoFechaDespacho, productosTotal);
+                                carrito = new Carrito(idCarrito, new Usuario(userId, userNombre, passwordUser, userEmail), pedido);
+                                return carrito;
+                            }
+                            else
+                            {
+                                pedido = new PedidoPendiente(pedidoId, emailPedido, direccionPedido, ordenDeCompra, pedidoFechaUltUso);
+                                carrito = new Carrito(idCarrito, new Usuario(userId, userNombre, passwordUser, userEmail), pedido);
+                                return carrito;
+                            }
                         }
                         reader.Close();
                     };
@@ -50,7 +95,42 @@ namespace Proyecto2.Model.Data
                 connection.Close();
             }
 
-            return carrito;
+            return false;
+        }
+        private List<ProductoCantidad> getProductosCarritoIdPedido(int idPedido)
+        {
+            List<ProductoCantidad> productosCantidad = new List<ProductoCantidad>();
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "select p.precio,p.id,p.impuesto,p.nombre,p.descripcion,p.cantidadDisponible,p.imagen, pp.cantidadComprado " +
+"FROM PedidoProducto pp inner join Producto p " +
+"on pp.idProducto = p.id where pp.idPedido = " + idPedido;
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        Producto producto;
+                        ProductoCantidad productoCantidad;
+                        while (reader.Read())
+                        {
+                            //double precioUnitario, int idProducto, double impuestoC, string nombre, string descripcion, int cantidadDisponible, string imagen
+                            double precioUnitario = reader.GetDouble(0);
+                            double impuestoC = Convert.ToDouble(reader.GetDecimal(2)) / 100;
+                            producto = new Producto(precioUnitario, reader.GetInt32(1), impuestoC,
+                                reader.GetString(3), reader.GetString(4), reader.GetInt32(5), reader.GetString(6));
+                            int cantidadComprado = reader.GetInt32(7);
+                            productoCantidad = new ProductoCantidad(producto, cantidadComprado);
+                            productosCantidad.Add(productoCantidad);
+                        }
+                        reader.Close();
+                    };
+                }
+                connection.Close();
+            }
+            return productosCantidad;
         }
 
         public void InsertarCarrito(Carrito carrito)
---
+++ GrupoMSEProyecto2/React2/Proyecto2.Tests/Data/CarritoDataTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Proyecto2.Model.Data;
+using Proyecto2.Model.Domain;
+
+namespace Proyecto2.Tests.Data
+{
+    public class CarritoDataTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public void ObtenerCarritoPorUsuario()
+        {
+            CarritoData carritoData = new CarritoData();
+            Object salida = carritoData.ObtenerCarritoPorUsuario(305060959);
+            bool prueba = salida is Carrito;
+            if (prueba)
+            {
+                //Es  Carrito
+                Carrito carrito = (Carrito)salida;
+                Assert.AreEqual("1",carrito.Id);
+                Assert.AreNotSame(256,carrito.Usuario.IdUsuario);
+                Assert.Positive(carrito.Pedido.OrdenDeCompra.ProductosCantidad.Count);
+
+                
+                Assert.IsTrue(carrito.Pedido is PedidoEntregado);
+
+            }
+            else
+            {
+                //No existe el carrito
+
+            }
+
+
+        }
+    }
+}
