12c645c1c1a68b56b9f7d5955260bd80f8325f84
Jeremy <jeremy.cm7445@gmail.com>
2019-07-18 10:48:15 -0600

    ExamenAmpliacionB73027

---
+++ B73027/ExamenAmpliacion/examen/Api/Controllers/ApiController.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace Api.Controllers
+{
+    public class ApiController : Controller
+    {
+
+        public static stubPeliculas stubPeliculas = new stubPeliculas();
+
+
+        [HttpGet("api/peliculas")]
+        public List<string> mostrar()
+        {
+            
+            return stubPeliculas.llenaPeliculas();
+
+        }
+
+    }
+}
---
+++ B73027/ExamenAmpliacion/examen/Model/domain/stubPeliculas.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class stubPeliculas
+    {
+
+        private List<string> peliculas= new List<string>();
+
+        public List<string> llenaPeliculas()
+        {
+
+
+
+            peliculas.Add("https://contenidos.enter.co/custom/uploads/2015/09/The-Fresh-Prince-1024x768.jpg");
+            peliculas.Add("https://upload.wikimedia.org/wikipedia/commons/6/6d/Leucanthemum_1024x768.jpg");
+            peliculas.Add("https://grupolemasa.com/wp-content/uploads/2017/05/MAR-28483-1024x768.jpg");
+            peliculas.Add("https://d3b4yo2b5lbfy.cloudfront.net/wp-content/uploads/wallpapers/af6953a10506-1024x768.jpg");
+            peliculas.Add("http://rioccadapt.com/wp-content/uploads/2017/06/Araucaria-Rio-Grande-do-Sul-State-1024x768.jpg");
+            peliculas.Add("https://www.cosmosim.org/resources/simus/MDR1_cl01_dm_evol_zoom_snap_320.jpg");
+            peliculas.Add("https://i0.wp.com/www.xeouradio.com/wp-content/uploads/2019/07/La-Mixteca-una-de-las-regiones-ms-afectada-por-la-cancula.jpg");
+            peliculas.Add("https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2017/01/148462643028v9cq7ytnu-dawn-armfield-1024x768.jpg");
+            peliculas.Add("http://www.fromearthtotheuniverse.org/webimages/Desktops/vela_1024x768.jpg");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQzuIaduiQsiXdmgPhLGBQW2r0ozCjBERphWczDJci-DPdU_Jf");
+            peliculas.Add("http://zonadelta.net/deltachan/src/1533604741957.jpg");
+            peliculas.Add("http://www.aventuradosblog.com/wp-content/uploads/2018/10/sierralago-12-1024x768.jpg");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOcSKHvcTGb3H2nktTJCjWfdBotSMKtv3cakLOgKlGQ8MvxBxn");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP9GXi15TolxIgm76g4TBhCD17bQ2_Uc2Wq2D5ShEP6SU64NybeQ");
+            peliculas.Add("http://hdqwalls.com/download/raven-girl-82-1024x768.jpg");
+            peliculas.Add("https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Al_Sheedakim%2C_Hacker_and_Programmer.JPG/1024px-Al_Sheedakim%2C_Hacker_and_Programmer.JPG");
+            peliculas.Add("https://vistarmagazine.com/wp-content/uploads/2019/07/cimafunk1-1024x768.jpg");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1O9_awlJOiR6EjjJxkMTW2pc1eDvrlpF-3ZHeVLdqwD_CljFJTQ");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-UIiXU_sXfOfN5uGnGFCQBCIRbS2bXaZsiYK3AFNcTcSxYsqX");
+            peliculas.Add("https://www.welivesecurity.com/wp-content/uploads/2019/07/zero-3a1000-1.jpg");
+            peliculas.Add("https://elduendedelaradio.files.wordpress.com/2012/01/luna_llena.jpg");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5slP_K8MpLjaRwgQUrjhnaX7DpPoxDGbQIfuNJ9qVUI15dBlyUA");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRO6opkIon_8VRrf04YVggOGqLi0_sKRJAS1rKxphNzP8yNYoe-w");
+            peliculas.Add("https://www.hogardelamadre.org/images/stories/multimedia/fondos_pantalla/Crucifijo_Barcenilla/cristocrucificado_1024_768.jpg");
+            peliculas.Add("http://www.aventuradosblog.com/wp-content/uploads/2018/01/inicio2018-6-1024x768.jpg");
+            peliculas.Add("https://pbs.twimg.com/media/D_clTezWwAEiyfL.jpg:large");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-NU6sWdC5f2yByKC3_ULXU0lxVx_UT1pd4EG4odehNKqNxi-yZw");
+            peliculas.Add("https://www.venuesplace.com/ficheros/WtKhB3hW/xdNGA3spP_t3.jpg.pagespeed.ic.wC1258mQIP.jpg");
+            peliculas.Add("https://imagenes247.com/wp-content/uploads/2018/11/825-1024x768-1024x768.jpg");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu0BGIV9dvIlzDFqhHgAKBDnay7qzf951wJOL6B0p-VlYwg3sIKQ");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFPAMx9sSk4NFQk0qi0wx8G82fkWzDPta7Hel-pPHsFNKSNko64g");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrDPnKZiC62yjsIjWOGaDQVw7MqtvmOyK3jMhIe_kmGodJf0xE5Q");
+            peliculas.Add("https://vistarmagazine.com/wp-content/uploads/2018/05/El-Micha-1024x768.jpg");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3epGRiZFxdsAeLuFXf4G-Qwn1ocG81CrSmxS_bx_8xp5qu7hdyw");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNEoAeDcdhkVEqSB_tO8vAHWLg-S_4RKErKY47L9oJLHzqvDo78A");
+            peliculas.Add("http://www.cabanasvillarivas.cl/images/galeria_lago/13262071_10208290087004286_452840073_o.jpg");
+            peliculas.Add("http://www.matriux.com/images/wallpapers/matriux-wp08.jpg");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSAYuxLU-oKitDfzo7BhfKC_heZWCWnMrBYrtHJ4f5hzvgaFcv-A");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnFmmNBTxZTjcIuICtJe2VxwS4t0YmiD1X_I8DBgGcEUDuMZxAVQ");
+            peliculas.Add("http://libertaddigital.news/wp-content/uploads/2019/07/dt.common.streams.StreamServer.jpeg-1.jpg");
+            peliculas.Add("https://cdns3.eltiempo.es/eltiempo/blog/noticias/2019/07/11154854/earth-64165_1280-1024x768.jpg");
+            peliculas.Add("http://www.adeluque.com/images/textos/f5aa26_Rubeosis%20iris.JPG");
+            peliculas.Add("https://sites.google.com/site/historiadelatierra05/_/rsrc/1327282156032/config/pp10.jpg.1327282155942.jpg");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp6ghblFzvFd2HdDl-spDQXs12-FrmlPtkGmvGgiaYxwChdBti4g");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2aXaccMRBs1wT7m3H9pR-Tq_VmsbbSSfYe4I41z862625oA1Kgw");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQZM6n1cdGe8T4EikMEn6pY1Yk8-o19QgcAe4Zu7SG5a2XmpNHNw");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxoBMdX5lJnHa6Ntk_bfo0B90SrSIhThm_bFJPkU7-dFyLRKIUTQ");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScVSR6B2ff-IO0Z2NxJGDxcpurz062BTxYFJTGPrCOXjOWvhgX");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhqa--bqbwOxC7PyFh89YmGF98QTJkonkfgv5WLfQMt58ssco89Q");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrE5X7GlVAjjuH6oYCSM4XFlkEy_RuByE1vqyFmU9qfeDvronk");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2IaumpbykWUxaqpt45wqEQB5zck83krWDKY63W8UCUeZgO5QB");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_PZrTEHalZuraY8CUIqVqC2iI15EK_cgNMwxqr2zIEFZPZV0V");
+            peliculas.Add("http://www.lapuebladelrio.es/export/sites/puebladelrio/.galleries/imagenes-noticias/2015/00_Goya_Oro.jpg");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZOPlRMBDUoszkYmLqnSQvAMcudNIHQG1RtL44Xbhcl5LOOdZp");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_MNmGpCkaNuuRQy93F-1gFzpUdNg_2CzW0_MFWAh4OiSAqrtKjw");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLoPVpWc5UAf4ppQEQASrvj6r5-dwMZeiQ7onqrFnqqS3nKCSk");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkw5C5BFgnmtyw2O9vQ_PW8tI2Ttp63Rwl01DcfAKukh56UBkr");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_Z1gipvazqdCFr5miFgr7kwh5RamrJvS9OQ6FlAQA7jkMnycxsg");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdMZoEKpYqQ1X4YU2ElaZjf1VWO4ruLNCtMIQgvyxY9IKQaYOx");
+            peliculas.Add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTUWDhkGNMIFKEq7CY8cMXSsUKYpEVhj891sK7heQ1f5NGq-fezg");
+
+            return peliculas;
+
+        }
+
+    }
+}
